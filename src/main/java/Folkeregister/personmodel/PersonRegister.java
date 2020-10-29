3fc398e1ddba2c54615db97dea4b8b5a29ca38fc
Hassan Abdi Samatar <xnabadoon@gmail.com>
2020-10-29 09:49:35 +0100

    Testing ssh key

--- src/main/java/Folkeregister/personmodel/PersonRegister.java
+++ src/main/java/Folkeregister/personmodel/PersonRegister.java
@@ -87,7 +87,7 @@ public class PersonRegister implements Serializable {
         people.clear();
         people.add(new Person("Ole Normann",30,15,12,1989,"m","15128972121","ole@gmail.com","47 40 42 91"));
         people.add(new Person("Kari Petersen",10,02,05,2010,"f","02051072213","susa@gmail.com","91 32 48 45"));
-        people.add(new Person("Hassan",28,19,10,1991,"m","19109199648","hassan@gmail.com","40 30 06 64"));
+        people.add(new Person("Hassan",28,19,10,1991,"m","19109199548","hassan@gmail.com","40 30 06 64"));
         people.add(new Person("Ali Akber",77,16,8,1942,"m","16084299549","Ali@ali.com","97 73 56 35"));
         people.add(new Person("Lidia Nani",117,11,12,1902,"f","11120249888","lidia@gmail.com","66 44 44 44"));
         people.add(new Person("Miski Leap Year",0,29,02,2020,"f","29022099861","lidia@gmail.com","46 32 67 51"));
